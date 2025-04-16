5607b5981b0a840cc7a1e020d45e3a555ecd7126
Lam Tieu Minh <minhlam1610.work@gmail.com>
2025-04-16 22:55:34 +0700

    Hoàn thiện front-end

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './App.jsx';
+import {ChakraProvider, ColorModeScript} from "@chakra-ui/react";
+import {BrowserRouter} from "react-router-dom";
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ChakraProvider>
+        <ColorModeScript initialColorMode="light" />
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
