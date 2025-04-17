11ee366e741eeea1e79c981c0a3990e333632cbc
Lam Tieu Minh <minhlam1610.work@gmail.com>
2025-04-17 08:19:49 +0700

    Fix bug and conflict

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
     import express from "express";
     import dotenv from "dotenv";
-    import path from 'path';
     import {connectDB} from "./config/db.js";
     import productRoutes from "./routes/product.route.js";
 
@@ -15,13 +14,6 @@
     app.use(express.json()); // dòng này là để cho phép nhận file Json trong phần request (req.body)
 
     app.use("/api/products", productRoutes);
-
-    if(process.env.NODE_ENV === "production"){
-        app.use(express.static(path.join(__dirname,"/frontend/dist")));
-        app.get("*", (req, res) => {
-            res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-        });
-    };
 
     app.listen(PORT, () => {
         connectDB();
