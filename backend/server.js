6eb6e3c927dcd2418875fcba69c2e5df87477421
Lam Tieu Minh <minhlam1610.work@gmail.com>
2025-04-17 18:16:38 +0700

    fix bug and con flict part 6

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,30 @@
-    import express from "express";
-    import dotenv from "dotenv";
-    import {connectDB} from "./config/db.js";
-    import productRoutes from "./routes/product.route.js";
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
 
-    dotenv.config();
+import { connectDB } from "./config/db.js";
 
-    const app = express();
+import productRoutes from "./routes/product.route.js";
 
-    const PORT = process.env.PORT || 5000;
+dotenv.config();
 
-    app.use(express.json()); // dòng này là để cho phép nhận file Json trong phần request (req.body)
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-    app.use("/api/products", productRoutes);
+const __dirname = path.resolve();
 
-    app.listen(PORT, () => {
-        connectDB();
-        console.log("Server started at http://localhost:" + PORT);
-    }); 
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
+});
