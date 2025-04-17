383cf358f0fac555666a59e78f770cf1ed2cb7f7
Lam Tieu Minh <minhlam1610.work@gmail.com>
2025-04-17 08:01:34 +0700

    Thêm file version và module cần để deploy

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,18 +1,18 @@
-import express, { Router } from "express";
-import {createProducts, getProducts, updateProduct, deleteProduct} from "../controllers/product.controller.js"
+    import express from "express";
+    import {createProducts, getProducts, updateProduct, deleteProduct} from "../controllers/product.controller.js";
 
-const router = express.Router();
+    const router = express.Router();
 
-// Phương thức này để tìm kiếm sản phẩm nhe
-router.get("/", getProducts);
+    // Phương thức này để tìm kiếm sản phẩm nhe
+    router.get("/", getProducts);
 
-// Phương thức này để thêm mới sản phẩm nhe
-router.post("/", createProducts);
+    // Phương thức này để thêm mới sản phẩm nhe
+    router.post("/", createProducts);
 
-// Phương thức update sản phẩm theo Id truyền vào
-router.put("/:id", updateProduct);
+    // Phương thức update sản phẩm theo Id truyền vào
+    router.put("/:id", updateProduct);
 
-// Phương thức xóa sản phẩm theo Id truyền vào
-router.delete("/:id", deleteProduct);
+    // Phương thức xóa sản phẩm theo Id truyền vào
+    router.delete("/:id", deleteProduct);
 
-export default router;
+    export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,29 @@
-import express from "express";
-import dotenv from "dotenv";
-import {connectDB} from "./config/db.js";
-import productRoutes from "./routes/product.route.js"
-dotenv.config();
+    import express from "express";
+    import dotenv from "dotenv";
+    import path from 'path';
+    import {connectDB} from "./config/db.js";
+    import productRoutes from "./routes/product.route.js";
 
-const app = express();
-const PORT = process.env.PORT;
+    dotenv.config();
 
-app.use(express.json()); // dòng này là để cho phép nhận file Json trong phần request (req.body)
+    const app = express();
+    const PORT = process.env.PORT || 5000;
 
-app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
-    connectDB();
-    console.log("Server started at http://localhost:" + PORT);
-}); 
+    const __dirname = path.resolve();
+
+    app.use(express.json()); // dòng này là để cho phép nhận file Json trong phần request (req.body)
+
+    app.use("/api/products", productRoutes);
+
+    if(process.env.NODE_ENV === "production"){
+        app.use(express.static(path.join(__dirname,"/frontend/dist")));
+        app.get("*", (req, res) => {
+            res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+        });
+    };
+
+    app.listen(PORT, () => {
+        connectDB();
+        console.log("Server started at http://localhost:" + PORT);
+    }); 
