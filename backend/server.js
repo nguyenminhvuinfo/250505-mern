6c6e3a08ae0b7626f4c7484465b9f81880339a22
Lam Tieu Minh <minhlam1610.work@gmail.com>
2025-04-17 08:41:49 +0700

    fix buf and conflict part4

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,13 @@
     import dotenv from "dotenv";
     import {connectDB} from "./config/db.js";
     import productRoutes from "./routes/product.route.js";
-    import cors from "cors";
 
     dotenv.config();
 
     const app = express();
 
-    app.use(cors(
-        {
-            origin: ["https://web-mern-stack-api.vercel.app/"],
-            methods: ["POST", "GET", "PUT", "DELETE"],
-            credentials: true
-        }
-    ));
-
     const PORT = process.env.PORT || 5000;
-    
+
     app.use(express.json()); // dòng này là để cho phép nhận file Json trong phần request (req.body)
 
     app.use("/api/products", productRoutes);
